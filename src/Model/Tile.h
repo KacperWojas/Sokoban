b34f0c0d52e9aecece1895ffd2471a2f04784f58
Cre4tos <cre4tos@proton.me>
2026-08-12 22:50:32 +0200

    tile enum

---
+++ src/Model/Tile.h
@@ -0,0 +1,21 @@
+//
+// Created by KacperWojas on 11/08/2026.
+//
+
+#ifndef SOKOBAN_TILE_H
+#define SOKOBAN_TILE_H
+
+
+enum class Tile
+{
+    EMPTY,
+    WALL,
+    GOAL,
+    PLAYER,
+    PLAYER_ON_GOAL,
+    BOX,
+    BOX_ON_GOAL,
+};
+
+
+#endif //SOKOBAN_TILE_H
